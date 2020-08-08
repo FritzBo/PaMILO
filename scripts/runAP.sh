27d763246e5fbaa7f03599a7c4ec8e00ff44cff8
Mirko H. Wagner <mirwagner@uos.de>
2020-08-08 14:43:54 +0200

    run

--- scripts/runAP.sh
+++ scripts/runAP.sh
@@ -7,6 +7,6 @@ python3 ../molp-algo/scripts/instanceFritzToVLP.py "$1"
 rm .temp >> /dev/null 2>&1
 /usr/bin/time -p ./cli/mco_cli $1.lp >> .temp
 rm $1_* >> /dev/null 2>&1
-/usr/bin/time -p ../fritsolve/bensolve-2.1.0/bensolve $1.vlp >> /dev/null
+/usr/bin/time -p $bensolve -Adual -adual $1.vlp >> /dev/null
 
 python3 ../molp-algo/scripts/diffEps.py .temp $1_img_p.sol
--- scripts/runAllFrit.sh
+++ scripts/runAllFrit.sh
@@ -2,7 +2,7 @@
 make -j16
 
 ulimit -v 12388608
-fileList=$(ls ../molp-algo/instances/fritzins | grep "ap_[0-9]*_[0-9]*_[0-9]*$")
+fileList=$(ls ../molp-algo/instances/fritzins | grep "ap_3_[4-8][0-9]*_[0-9]*$\|ap_4_[1-4][0-9]_[0-9]*$\|ap_5_[1-2][0-9]_[0-9]*$\|ap_6_[0-9]_[0-9]*$\|ap_6_1[0-2]_[0-9]*$")
 for file in $fileList
 do
 	#echo $file
@@ -10,5 +10,5 @@ do
 	then
 		echo $file
 		./scripts/runAP.sh ../molp-algo/instances/fritzins/$file
-	fi	
+	fi
 done
